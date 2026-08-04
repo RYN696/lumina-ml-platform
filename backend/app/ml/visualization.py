4d3e637105e38163d63553c62870d9ec8481e381
RYN696 <rayenghrairi53@gmail.com>
2026-08-04 15:09:11 +0100

    Complete visualization engine v1

--- backend/app/ml/visualization.py
+++ backend/app/ml/visualization.py
@@ -40,3 +40,54 @@ def generate_histograms(dataframe: pd.DataFrame) -> list[str]:
         generated_files.append(file_path)
 
     return generated_files
+
+def generate_boxplots(dataframe: pd.DataFrame) -> list[str]:
+    """
+    Generate boxplot plots for all numerical analysis columns.
+    """
+    create_output_directory()
+
+    numerical_columns = get_analysis_columns(dataframe)
+    generated_files = []
+
+    for column in numerical_columns:
+        plt.figure(figsize=(8, 5))
+
+        sns.boxplot(x=dataframe[column])
+
+        plt.title(f"Boxplot of {column}")
+
+        plt.tight_layout()
+
+        file_path = f"reports/figures/{column}_boxplot.png"
+
+        plt.savefig(file_path)
+        plt.close()
+
+        generated_files.append(file_path)
+
+    return generated_files
+
+def generate_correlation_heatmap(dataframe: pd.DataFrame) -> str:
+    """
+    Generate a correlation heatmap for all numerical analysis columns.
+    """
+    create_output_directory()
+
+    numerical_columns = get_analysis_columns(dataframe)
+    correlation_matrix = dataframe[numerical_columns].corr()
+
+    plt.figure(figsize=(8, 6))
+
+    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f",square=True)
+
+    plt.title("Correlation Heatmap")
+
+    plt.tight_layout()
+
+    file_path = "reports/figures/correlation_heatmap.png"
+
+    plt.savefig(file_path)
+    plt.close()
+
+    return file_path
--- backend/test_visualization.py
+++ backend/test_visualization.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 from app.ml.visualization import generate_histograms
-
+from app.ml.visualization import generate_boxplots
+from app.ml.visualization import generate_correlation_heatmap
 
 data = pd.read_csv(
     "../datasets/iris.csv"
@@ -12,3 +13,12 @@ files = generate_histograms(data)
 
 
 print(files)
+
+boxplot_files = generate_boxplots(data)
+
+
+print(boxplot_files)
+
+heatmap_file = generate_correlation_heatmap(data)   
+
+print(heatmap_file)
